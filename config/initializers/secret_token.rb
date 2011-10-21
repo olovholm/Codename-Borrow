3fea0e48884836afeb84b3cd249e5beb0c92fb87
Ola Loevholm <ola@lovholm.net>
2011-10-21 12:04:26 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Borrow::Application.config.secret_token = '8c6f155ac9ae216f7fd69c8c0578b46e8e8361ebbc501a5cd08f72e5d9390289fc6f7ba53a3c2f533f668d2fe71007c84eb2454c275e959be7a539b762412a34'
