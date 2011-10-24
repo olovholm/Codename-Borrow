c022b2446fb7fb084eef0a23a28292e98cb71681
Ola Loevholm <ola@lovholm.net>
2011-10-24 17:24:51 +0200

    Quick-fix of charset error with Heroku

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,7 +14,7 @@ class UserController < ApplicationController
       flash[:notice] = "Brukeren din er lagret. Sjekk e-posten din for bekreftelseslink"
       redirect_to :action => "home"
     else 
-      flash[:error] = "<em>Vi kom over noen feil når vi skulle lagre brukeren din:</em><br />"
+      flash[:error] = "<em>Vi kom over noen feil naar vi skulle lagre brukeren din:</em><br />"
       @user.errors.each {|attr,msg| flash[:error] << "#{attr} #{msg} <br/>" }
       render :action => "new"
     end
@@ -28,7 +28,7 @@ class UserController < ApplicationController
         flash[:error] = "You could not be logged in"
         redirect_to :action => "login"
       else
-        flash[:success] = "Du er nå logget inn #{@user.username}"
+        flash[:success] = "Du er naa logget inn #{@user.username}"
         session[:user_id] = @user.id
         session[:username] = @user.username
         redirect_to :action => "home"
@@ -38,7 +38,7 @@ class UserController < ApplicationController
   
   def logout 
     reset_session 
-    flash[:success] = "Du er nå fullstendig logget ut"
+    flash[:success] = "Du er naa fullstendig logget ut"
     redirect_to :controller => "pages", :action => "index"
     
   end
