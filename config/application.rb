99dbfa11735c7aae9359a17aba849e3196582386
Ola Loevholm <ola@lovholm.net>
2011-10-26 23:57:20 +0200

    Added very liberal rules on what should be precompiled. Now including all .css and .js files

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module Borrow
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile << '*.js'
+    config.assets.precompile << '*.css'
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
